a41a7af2e858aa3ce5a7f977928d5f973af66206
Harut <yes@harutune.name>
2016-09-30 20:35:05 +0300

    fixed dependencies

--- spot.js
+++ spot.js
@@ -2,7 +2,7 @@
 ;(function (factory) {
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD. Register as an anonymous module.
-		define(['jquery', 'jquery-mousewheel'], factory);
+		define(['jquery'], factory);
 	} else if (typeof exports === 'object') {
 		// Node/CommonJS style for Browserify
 		module.exports = factory;
